689c745e8d801cfca3096485c33b998c8a450411
Parham <parham2002sh@gmail.com>
2022-06-29 15:47:21 -0700

    bouncy rectangle

--- image_processing/image_processing/detector.py
+++ image_processing/image_processing/detector.py
@@ -26,32 +26,42 @@ import cv2 as cv
 
 import cv2 as cv
 print(cv.__version__)
-dispW = 640 * 2
-dispH = 480 * 2
+dispW = 640
+dispH = 480
 flip = 2
+
+x = y = 0
+x_inc = y_inc = -2
+
+rect = (60, 40)
 
 color = (0, 0, 255)
 
 cam = cv.VideoCapture(0)
 
+
 while True:
     ret, frame = cam.read()
 
+    x = x + x_inc
+    y = y + y_inc
 
+    if x <= 0:
+         x_inc = 10
+    elif x + rect[0] >= frame.shape[1]:
+        x_inc = -10
 
-    frame = cv.resize(frame, (dispW, dispH))
-    # frame = cv.rectangle(frame, dim, (dim[0] + 40, dim[1] + 80), color, 2)
+    if y <= 0:
+        y_inc = 10
+    elif y + rect[1] >= frame.shape[0]:
+        y_inc = -10
+
+    print (f'x: {x}, y: {y}')
+    # frame = cv.resize(frame, (dispW, dispH))
+    frame = cv.rectangle(frame, (0 + x, 0 + y), (rect[0] + x,  rect[1] + y), color, 2)
 
     cv.imshow('Camera', frame)
     cv.moveWindow('Camera', 200, 200)
-
-
-    width = int(frame.shape[1])
-    length = int(frame.shape[0])
-
-    dim = (width, length)
-
-    print(f'{dim}')
 
     if cv.waitKey(1) == ord('q'):
         break
